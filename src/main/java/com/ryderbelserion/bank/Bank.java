3924728a8e8648caf27be4b018282b3588c47f8c
Ryder Belserion <no-reply@ryderbelserion.com>
2024-09-23 02:11:30 -0400

    add debug

--- src/main/java/com/ryderbelserion/bank/Bank.java
+++ src/main/java/com/ryderbelserion/bank/Bank.java
@@ -6,11 +6,11 @@ public class Bank extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getComponentLogger().info("<red>Bank is now enabled!");
     }
 
     @Override
     public void onDisable() {
-
+        getComponentLogger().info("<red>Bank is now disabled!");
     }
 }
